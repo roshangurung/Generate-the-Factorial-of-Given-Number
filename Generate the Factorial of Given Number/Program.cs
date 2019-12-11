4b3f7f67f1637f946b1c08283cf70ff25a2a67c4
roshangurung <r.gurung1992@gmail.com>
2019-12-10 19:36:15 -0800

    Initial commit

---
+++ Generate the Factorial of Given Number/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace factorial
+{
+    class Program////Using for loop, factorial of a number is obtained from the result of multiplying a series of descending natural numbers. Print the factorial of given number.///
+    {
+        static void Main(string[] args)
+        {
+            int i, number, fact;
+            Console.WriteLine("Enter the Number");
+            number = int.Parse(Console.ReadLine());
+            fact = number;
+            for (i = number - 1; i >= 1; i--)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine("\nFactorial of Given Number is: " + fact);
+            Console.ReadLine();
+
+        }
+    }
+}
+///Output: 
+///Enter the number: 2
+///Factorial of Given Number is: 2
